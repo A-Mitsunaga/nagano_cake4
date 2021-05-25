ef68843c4a2bb270eb7da709396e4c3fda1f1fb7
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-25 10:30:54 +0000

    [Add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c9bbc5bf3cce4f4b9b56b623e636074df040d23d9cb76d2c6df3c4ccbd8e8d79c67a151576a0c771cf22160d0b4a78d518426033f8e3eeae932b7612b87875a8'
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
  
  namespace :admin do
  resources :items
+ post '/items/new' => 'items#create'
 end
   devise_for :customers
 end
