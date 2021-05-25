c2d41fc54bdde3ba1a438a6e786b5975ea330837
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-25 10:19:09 +0000

    [Add]itemテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_084139) do
+ActiveRecord::Schema.define(version: 2021_05_25_095516) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2021_05_25_084139) do
     t.boolean "admin", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
 end
