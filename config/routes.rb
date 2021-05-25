2fe93fe8262a42f750e28f7c88787aa3cadb9694
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-25 10:50:13 +0000

    [Add]ジャンル追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about'
   get '/admin' => 'homes#top'
   #post '/customers/sign_up' => 'devise/registrations#create'
- 
+
  namespace :admin do
  resources :items
+ resources :genres, only: [:index, :create, :edit, :uodate]
  post '/items/new' => 'items#create'
+ post '/genres/new' => 'genres#create'
+
 end
   devise_for :customers
 end
